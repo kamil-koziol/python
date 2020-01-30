8ff5ae3baab6dc3088cb09bce2b708ef6fb28e32
Kamil <panpouran@gmail.com>
2020-01-30 11:00:45 +0100

    backup

---
+++ Dodatkowe/szkola.py
@@ -0,0 +1,9 @@
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+print(bubbleSort([1,7,4,2,6,3,2]))
