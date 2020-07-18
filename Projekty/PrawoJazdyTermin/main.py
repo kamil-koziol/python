45e9dad10efcaa741aae061bcda9610060460bb0
Kamil Kozioł <38388156+kamil-koziol@users.noreply.github.com>
2020-07-18 10:45:27 +0200

    Update main.py

--- Projekty/PrawoJazdyTermin/main.py
+++ Projekty/PrawoJazdyTermin/main.py
@@ -5,14 +5,10 @@ from datetime import date
 options = Options()
 options.headless = True
 
-# driver = UMDriver(options)
-# free_days = driver.get_days()
-# searched_dates = driver.check_days(free_days)
-# print(searched_dates)
-
-days_in_current_month = UMDriver.month_days(date.today().month)
-wolne_dni = [i for i in range(1, days_in_current_month)]
-print(wolne_dni)
+driver = UMDriver(options)
+free_days = driver.get_days()
+searched_dates = driver.check_days(free_days)
+print(searched_dates)
 
 
 
